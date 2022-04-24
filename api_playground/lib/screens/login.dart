c01081aace8fd8e29a88d899a647bc1e578b3328
Nicholas Moen <arcanemachine@tutanota.com>
2022-04-24 16:21:47 -0600

    Refactor login form to fix issues with rebuilding

--- api_playground/lib/screens/login.dart
+++ api_playground/lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+// ignore: unnecessary_import
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -40,128 +41,129 @@ class LoginWidget extends StatefulWidget {
 }
 
 class _LoginWidgetState extends State<LoginWidget> {
-  @override
-  Widget build(BuildContext context) {
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    bool _isLoading = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var _isLoading = false;
 
-    // controllers
-    final _usernameController =
-      TextEditingController.fromValue(const TextEditingValue(text: "user"));
-    final _passwordController =
-      TextEditingController.fromValue(const TextEditingValue(text: "password"));
+  void _isLoadingSet(bool val) {
+    setState(() { _isLoading = val; });
+  }
 
+  // controllers
+  final _usernameController =
+  TextEditingController.fromValue(const TextEditingValue(text: "user"));
+  final _passwordController =
+  TextEditingController.fromValue(const TextEditingValue(text: "password"));
 
-    // widgets
-    Widget _loginForm() {
-      final _globals = context.read<Globals>();
+  // widgets
+  Widget _loginForm() {
+    final _globals = context.read<Globals>();
 
-      final ButtonStyle _buttonStyle = ElevatedButton.styleFrom(
-        textStyle: const TextStyle(fontSize: 20.0),
-        minimumSize: const Size(300, 60),
-      );
+    final ButtonStyle _loginButtonStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20.0),
+      minimumSize: const Size(300, 60),
+    );
 
-      void _handleSubmit() async {
-        if (kDebugMode) print("_handleSubmit()");
-        final String _username = _usernameController.text;
-        final String _password = _passwordController.text;
+    void _handleSubmit() async {
+      // if (kDebugMode) print("_handleSubmit()");
+      final String _username = _usernameController.text;
+      final String _password = _passwordController.text;
 
-        // set loading status
-        _isLoading = true;
+      // set loading status
+      _isLoadingSet(true);
 
-        // attempt to login
-        final Uri _loginUrl = Uri.parse(_globals.urls.login);
-        final http.Response _response = await http.post(_loginUrl, body: {
-          "username": _username,
-          "password": _password,
-        });
+      // attempt to login
+      final Uri _loginUrl = Uri.parse(_globals.urls.login);
+      final http.Response _response = await http.post(_loginUrl, body: {
+        "username": _username,
+        "password": _password,
+      });
 
-        // reset loading status
-        _isLoading = false;
+      // reset loading status
+      _isLoadingSet(false);
 
-        if (_response.statusCode == 200) {
-          final decodedResponse = jsonDecode(_response.body);
+      if (_response.statusCode == 200) {
+        final decodedResponse = jsonDecode(_response.body);
 
-          // update state
-          await secureStorage  // save token to secure storage
+        // update state
+        await secureStorage  // save token to secure storage
             .write('user_api_token', decodedResponse['key']);
-          sharedPrefs.isLoggedIn = true; // set login status
+        sharedPrefs.isLoggedIn = true; // set login status
 
-          // login
-          helpers.login(context);
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              // content: Text("Form submitted"),
-              content: Text(
+        // login
+        helpers.login(context);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
                 "Error ${_response.statusCode}: ${_response.reasonPhrase}"
-              ),
-              // content: Text("loginUrl: $_loginUrl"),
-              action: SnackBarAction(
-                label: 'OK',
-                onPressed: () {},
-              ),
             ),
-          );
-        }
-      }
-
-      return Form(
-        key: _formKey,
-        child: Container(
-          constraints: const BoxConstraints(minWidth: 300, maxWidth: 300),
-          child: Column(
-            children: [
-              const SizedBox(height: 16.0),
-              TextFormField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  labelText: "Username *",
-                ),
-                validator: (val) {
-                  if (val == null || val.isEmpty) {
-                    return "This field must not be empty.";
-                  }
-                  return null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              ),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.key),
-                  labelText: "Password *",
-                ),
-                validator: (val) {
-                  if (val == null || val.isEmpty) {
-                    return "This field must not be empty.";
-                  }
-                  return null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              ),
-              const SizedBox(height: 30.0),
-              Center(
-                child: ElevatedButton(
-                  child: _isLoading
-                    ? const Text("Loading...")
-                    : const Text("Login"),
-                  style: _buttonStyle,
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      _handleSubmit();
-                    }
-                  },
-                ),
-              ),
-            ],
+            action: SnackBarAction(
+              label: 'OK',
+              onPressed: () {},
+            ),
           ),
-        ),
-      );
+        );
+      }
     }
 
+    return Form(
+      key: _formKey,
+      child: Container(
+        constraints: const BoxConstraints(minWidth: 300, maxWidth: 300),
+        child: Column(
+          children: [
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                labelText: "Username *",
+              ),
+              validator: (val) {
+                if (val == null || val.isEmpty) {
+                  return "This field must not be empty.";
+                }
+                return null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.key),
+                labelText: "Password *",
+              ),
+              validator: (val) {
+                if (val == null || val.isEmpty) {
+                  return "This field must not be empty.";
+                }
+                return null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            const SizedBox(height: 30.0),
+            Center(
+              child: ElevatedButton(
+                child: !_isLoading
+                  ? const Text("Login")
+                  : const CircularProgressIndicator(),
+                style: _loginButtonStyle,
+                onPressed: _isLoading ? null : () async {
+                  if (_formKey.currentState!.validate()) {
+                    _handleSubmit();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: <Widget>[
@@ -178,4 +180,3 @@ class _LoginWidgetState extends State<LoginWidget> {
     );
   }
 }
-
